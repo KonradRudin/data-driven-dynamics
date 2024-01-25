9b5a0da890abb4449c269c63d8ab332cdf05c306
anheinle <anheinle@ethz.ch>
2024-01-25 14:28:32 +0100

    CleanUp

--- Tools/parametric_model/Test_FixedWing/Params_estimation.py
+++ Tools/parametric_model/Test_FixedWing/Params_estimation.py
@@ -18,118 +18,56 @@ sys.path.append('/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/par
 from generate_parametric_model import *
 
 import logging
-
-# Set the logging level to WARNING
+# Set the logging level 
 logging.basicConfig(level=logging.INFO)
-
-# Conditions for further processing
-def check_analysis_possible(ulog, px4_ulog, valid_vehicles):
-    continue_processing = True
-    # No further processing can be done if ...
-
-    # ...if log file is corrupted and information is missing 
-    if ulog.file_corruption:
-        print('WARNING: Corrupt Log File')
-        continue_processing = False
-
-    # ...if duration of log file is 0s
-    m ,s = divmod(int((ulog.last_timestamp - ulog.start_timestamp)/1e6), 60)
-    if m == 0 and s == 0 :
-        print('ERROR: Logging duration is 0s')
-        continue_processing = False
-
-    # ... if type of vehicle is wrong
-    if ulog.initial_parameters['MAV_TYPE'] not in valid_vehicles:
-        print("ERROR_1: This flight log file from vehicle type " + px4_ulog.get_mav_type()+ " cannot be analyzed")
-        continue_processing = False
-
-    return continue_processing
-
-def sys_id_quadrotor(file_name, info_dict):
-    
-    info_dict['log_path'] = file_name
-
-    # predict model parameters
-    start_model_estimation(**(info_dict))
-
-    return 
-
-def params_estimation(path_to_folder, mav_type, info_dict):
-    i = 0
-    with os.scandir(path_to_folder) as entries:
-        for entry in entries:
-            print()
-            print(i, entry.name)
-
-            # file name of log file
-            file_name = os.path.join(path_to_folder, entry.name)
-
-            try:
-                ulog = ULog(file_name)
-                px4_ulog = PX4ULog(ulog)
-                px4_ulog.add_roll_pitch_yaw() 
-
-            except:
-                raise Exception("ULog has wrong format and won't be analyzed")
-
-
-            if check_analysis_possible(ulog, px4_ulog, valid_vehicle_type) is False:
-                raise Exception("ULog doesn't meet processing-conditions and won't be analyzed")
-            
-            #sys_id_quadrotor(file_name, info_dict)
-            # If quadrotor -> process further
-            if ulog.initial_parameters['MAV_TYPE'] == mav_type: # and ulog.initial_parameters['SYS_AUTOSTART'] == 1206900:
-                try:
-
-                    sys_id_quadrotor(file_name, info_dict)
-                
-
-                except:
-                    print('======== System Identification doesnt work', file_name)
-                    # destination_file_path = os.path.join('/home/anna/Documents/flight_log_usb_stick/SysID/Quadrotors/good/SYS_ID_NoFurtherProcess', entry.name)
-                    # shutil.copy(file_name, destination_file_path)
-
-            else:
-                print('Not a Fixed Wing')
-            i+=1
 
 # ==============================================================================================================================================
 
-path_to_folder = '/home/anna/Documents/System Identification/Test'
-
-# multicopters
-valid_vehicle_type = [1,22]
-
-data_selction = False #'interactive'
-
-# === Linear Model
-# config = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/parametric_model/configs/fixedwing_model_roll.yaml'
-# Linear Model with old topic
-# config = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/parametric_model/configs/fixedwing_model_roll_old.yaml'
-
-# === NonLinear Model
-# config = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/parametric_model/configs/fixedwing_model_roll _nonlinear.yaml'
-# Nonlinear Model with old uorb topics
-config = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/parametric_model/configs/fixedwing_model_roll _nonlinear_old_topic.yaml'
-
-
-mav_type = 22 # FixedWing
-
 info_dict = {'log_path': None , 
-            'data_selection': data_selction, 
-            'config': config, 
+            'data_selection': None, 
+            'config': None, 
             'normalization': True,
             'plot' : True, 
             'selection_var': 'none',
             'extraction': False
             }
 
+""" 
+DATA_SELECTION argument: 
+    - False (whole section of the log is used)
+    - interactive (select data interactively using vpselector)
+"""
+info_dict['data_selection']  = 'interactive'
 
+""" 
+CONFIG argument: 
+    - Linear Model (L)
+            with old/new uorb topics
+    - Nonlinear Model (NL)
+            with old/new uorb topics
+"""
+# CONFIG: L, new
+# config = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/parametric_model/configs/fixedwing_model_roll.yaml'
+# CONFIG: L, old
+# config = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/parametric_model/configs/fixedwing_model_roll_old.yaml'
+# CONFIG: NL, new
+info_dict['config'] = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/parametric_model/configs/fixedwing_model_roll_nonlinear.yaml'
+# CONFIG: NL, old
+# config = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/parametric_model/configs/fixedwing_model_roll _nonlinear_old_topic.yaml'
 
-# Loong log data
-info_dict['log_path'] = '/home/anna/Documents/System_Identification/Loong_logs/Log_snippets/006/006_check/same_log_as_estimation/e89126_selection003.csv'
+""" 
+The Log file contains all data needed for the system identification of the specified model as defined in its config file
+"""
+# LOG-PATH: Estimation of Simulation Data
+# info_dict['log_path'] = '/home/anna/Documents/System_Identification/Simulation_data/With_aileron_acutators/13_54_51_selection01.csv'
+# LOG-PATH: Estimation of Simulation data - locked aileron
+info_dict['log_path'] = '/home/anna/Documents/System_Identification/Simulation_data/13_57_34.ulg'
+# === LOG-PATH: Loong log data
+# info_dict['log_path'] = '/home/anna/Documents/System_Identification/Loong_logs/Log_snippets/006/006_check/same_log_as_estimation/e89126_selection003.csv'
+
+# ==============================================================================================================================================
+
 # predict model parameters
-
 start_model_estimation(**(info_dict))
 
 
--- Tools/parametric_model/Test_FixedWing/Params_prediction.py
+++ Tools/parametric_model/Test_FixedWing/Params_prediction.py
@@ -20,7 +20,7 @@ import csv
 import math
 
 
-data_selction = False
+data_selction = 'interactive' #False
 
 
 info_dict = {'log_path': None , 
@@ -28,44 +28,46 @@ info_dict = {'log_path': None ,
             'config': None, 
             'model_results': None}
 
-results = []
-i = 0
 
-# plt.ioff()
-def show_nothing():
-    pass
-# plt.show = show_nothing
 
 # === CONFIG ===========================================
 # info_dict['config'] = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/parametric_model/configs/fixedwing_model_roll.yaml'
-# info_dict['config'] = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/parametric_model/configs/fixedwing_model_roll _nonlinear.yaml'
-info_dict['config'] = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/parametric_model/configs/fixedwing_model_roll _nonlinear_old_topic.yaml'
+info_dict['config'] = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/parametric_model/configs/fixedwing_model_roll_nonlinear.yaml'
+# info_dict['config'] = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/Tools/parametric_model/configs/fixedwing_model_roll _nonlinear_old_topic.yaml'
+
 
 # === MODEL RESULTS =====================================
 # == no t_d
-# info_dict['model_results'] = '/home/anna/Workspaces/ddd_ws/src/data-driven-dynamics/model_results_estimation_RollModel/simple_fixedwing_model_2024-01-22-09-11-49.yaml'
-
+# info_dict['model_results'] = 'model_results_estimation_RollModel/FixedWingRollModel_Coeff_estimated/simple_fixedwing_model_simulation_L.yaml'
 # == with t_d
 # From simualation data
-# info_dict['model_results'] = 'model_results_estimation_RollModel/simple_fixedwing_model_2024-01-24-10-27-57.yaml'
+info_dict['model_results'] = 'model_results_estimation_RollModel/FixedWingRollModel_Coeff_estimated/simple_fixedwing_model_simulation_NL.yaml'
 # Loong 006
-info_dict['model_results'] = 'model_results_estimation_RollModel/simple_fixedwing_model_2024-01-24-14-29-41.yaml'
+# info_dict['model_results'] = 'model_results_estimation_RollModel/FixedWingRollModel_Coeff_estimated/simple_fixedwing_model_Loong_006_NL.yaml'
+# info_dict['model_results'] = 'model_results_estimation_RollModel/simple_fixedwing_model_2024-01-25-08-46-13.yaml'
+
+# info_dict['model_results'] = 'model_results_estimation_RollModel/simple_fixedwing_model_2024-01-25-10-52-45.yaml'
+
+# info_dict['model_results'] = 'model_results_estimation_RollModel/FixedWingRollModel_Coeff_estimated/simple_fixedwing_model_simulation_locked_left_aileron_NL.yaml'
+
+# === LOG ===============================================
+info_dict['log_path'] = '/home/anna/Documents/System_Identification/Simulation_data/13_54_51.ulg'
 
 
-# info_dict['log_path'] = '/home/anna/Documents/System_Identification/Simulation_data/14_47_07.ulg'
-# start_model_prediction(**(info_dict))
+start_model_prediction(**(info_dict))
 
 
 # path_to_folder = '/home/anna/Documents/System_Identification/Simulation_data/With_aileron_acutators'
-path_to_folder = '/home/anna/Documents/System_Identification/Loong_logs/Log_snippets/006/006_check/same_log_as_estimation'
+# path_to_folder = '/home/anna/Documents/System_Identification/Loong_logs/Log_snippets/006/006_check/same_log_as_estimation'
+# path_to_folder = '/home/anna/Documents/System_Identification/Simulation_data/One_locked_aileron/snippets'
 
-with os.scandir(path_to_folder) as entries:
-        for entry in entries:
-            print()
-            print(entry.name)
+# with os.scandir(path_to_folder) as entries:
+#         for entry in entries:
+#             print()
+#             print(entry.name)
 
-            # file name of log file
-            file_name = os.path.join(path_to_folder, entry.name)
-            info_dict['log_path'] = file_name
-            start_model_prediction(**(info_dict))
+#             # file name of log file
+#             file_name = os.path.join(path_to_folder, entry.name)
+#             info_dict['log_path'] = file_name
+#             start_model_prediction(**(info_dict))
 
